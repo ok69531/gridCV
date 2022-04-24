596621b5c76093acd745fb7df474cd579cd1c16b
soyeungjo <ok69531@gmail.com>
2022-04-24 16:56:55 +0900

    updated

--- logistic_gridcv.py
+++ logistic_gridcv.py
@@ -1,3 +1,77 @@
+#%%
+import time
+import random
+import openpyxl
+import warnings
+
+import pandas as pd
+import numpy as np
+from sqlalchemy import column 
+
+from tqdm import tqdm
+from rdkit import Chem
+from rdkit.Chem import MACCSkeys
+from matplotlib import pyplot as plt
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+import scipy.stats as stats
+
+pd.set_option('mode.chained_assignment', None)
+warnings.filterwarnings("ignore")
+
+
+#%%
+'''
+    read data
+'''
+
+path = 'C:/Users/SOYOUNG/Desktop/toxic/data/oecd_echemportal/Preprocessed data/lc50_split/'
+
+mgl = pd.read_excel(path + 'train_mgl.xlsx', sheet_name = 'Sheet1')
+ppm = pd.read_excel(path + 'train_ppm.xlsx', sheet_name = 'Sheet1')
+
+
+def Smiles2Fing(smiles):
+    ms_tmp = [Chem.MolFromSmiles(i) for i in smiles]
+    ms_none_idx = [i for i in range(len(ms_tmp)) if ms_tmp[i] == None]
+    
+    ms = list(filter(None, ms_tmp))
+    
+    maccs = [MACCSkeys.GenMACCSKeys(i) for i in ms]
+    maccs_bit = [i.ToBitString() for i in maccs]
+    
+    fingerprints = pd.DataFrame({'maccs': maccs_bit})
+    fingerprints = fingerprints['maccs'].str.split(pat = '', n = 167, expand = True)
+    fingerprints.drop(fingerprints.columns[0], axis = 1, inplace = True)
+    
+    colname = ['maccs_' + str(i) for i in range(1, 168)]
+    fingerprints.columns = colname
+    fingerprints = fingerprints.astype(int).reset_index(drop = True)
+    
+    return ms_none_idx, fingerprints
+
+
+mgl_drop_idx, mgl_fingerprints = Smiles2Fing(mgl.SMILES)
+mgl_y = mgl.value.drop(mgl_drop_idx).reset_index(drop = True)
+mgl_y = pd.DataFrame({'value': mgl_y,
+                     'category': pd.cut(mgl_y, bins = [0, 0.5, 2.0, 10, 20, np.infty], labels = range(5))})
+# mgl_y = pd.DataFrame({'value': mgl_y,
+#                      'category': pd.qcut(mgl_y, 5, labels = range(5))})
+mgl_y['category'].value_counts().sort_index()
+mgl_y['category'].value_counts(normalize = True).sort_index()
+
+
+ppm_drop_idx, ppm_fingerprints = Smiles2Fing(ppm.SMILES)
+ppm_y = ppm.value.drop(ppm_drop_idx).reset_index(drop = True)
+ppm_y = pd.DataFrame({'value': ppm_y,
+                      'category': pd.cut(ppm_y, bins = [0, 100, 500, 2500, 20000, np.infty], labels = range(5))})
+# ppm_y = pd.DataFrame({'value': ppm_y,
+#                       'category': pd.qcut(ppm_y, 5, labels = range(5))})
+
+
+#%%
 from itertools import product
 from collections.abc import Iterable
 
@@ -30,59 +104,86 @@ logit_params = {'random_state': [0],
 params = ParameterGrid(logit_params)
 
 
-
 #%%
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import KFold
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+from sklearn.model_selection import KFold
 
 
-def gridCV(model, data_x, data_y, param_dict):
+kf = KFold(n_splits = 5)
+
+result_ = []
+metrics = ['macro_precision', 'weighted_precision', 'macro_recall', 
+           'weighted_recall', 'macro_f1', 'weighted_f1', 'accuracy']
+train_metrics = list(map(lambda x: 'train_' + x, metrics))
+val_metrics = list(map(lambda x: 'val_' + x, metrics))
+
+
+for i in tqdm(range(len(params))):
     
-    kf = KFold(n_splits = 5)
-    params = ParameterGrid(param_dict)
-    
-    result_ = []
-    metrics_ = ['parameters', 'macro_precision', 'weighted_precision', 
-                'macro_recall', 'weighted_recall', 'macro_f1', 'weighted_f1', 'accuracy']
-    
-    for i in tqdm(range(len(params))):
+    train_macro_precision_, train_weighted_precision_ = [], []
+    train_macro_recall_, train_weighted_recall_ = [], []
+    train_macro_f1_, train_weighted_f1_ = [], []
+    train_accuracy_ =[]
+
+    val_macro_precision_, val_weighted_precision_ = [], []
+    val_macro_recall_, val_weighted_recall_ = [], []
+    val_macro_f1_, val_weighted_f1_ = [], []
+    val_accuracy_ =[]
+
+    for train_idx, val_idx in kf.split(mgl_fingerprints):
+        train_x, train_y = mgl_fingerprints.iloc[train_idx], mgl_y.category.iloc[train_idx]
+        val_x, val_y = mgl_fingerprints.iloc[val_idx], mgl_y.category.iloc[val_idx]
         
-        macro_precision_ = []
-        weighted_precision_ = []
-        macro_recall_ = []
-        weighted_recall_ = []
-        macro_f1_ = []
-        weighted_f1_ = []
-        accuracy_ =[]
+        model = LogisticRegression(**params[i])
+        model.fit(train_x, train_y)
+        
+        train_pred = model.predict(train_x)
+        val_pred = model.predict(val_x)
+        
+        train_macro_precision_.append(precision_score(train_y, train_pred, average = 'macro'))
+        train_weighted_precision_.append(precision_score(train_y, train_pred, average = 'weighted'))
+        train_macro_recall_.append(recall_score(train_y, train_pred, average = 'macro'))
+        train_weighted_recall_.append(recall_score(train_y, train_pred, average = 'weighted'))
+        train_macro_f1_.append(f1_score(train_y, train_pred, average = 'macro'))
+        train_weighted_f1_.append(f1_score(train_y, train_pred, average = 'weighted'))
+        train_accuracy_.append(accuracy_score(train_y, train_pred))
 
-        for train_idx, test_idx in kf.split(data_x):
-            train_x, train_y = data_x.iloc[train_idx], data_y.iloc[train_idx]
-            test_x, test_y = data_x.iloc[test_idx], data_y.iloc[test_idx]
-            
-            model_ = model(**params[i])
-            model_.fit(train_x, train_y)
-            pred = model_.predict(test_x)
-            
-            macro_precision_.append(precision_score(test_y, pred, average = 'macro'))
-            weighted_precision_.append(precision_score(test_y, pred, average = 'weighted'))
-            macro_recall_.append(recall_score(test_y, pred, average = 'macro'))
-            weighted_recall_.append(recall_score(test_y, pred, average = 'weighted'))
-            macro_f1_.append(f1_score(test_y, pred, average = 'macro'))
-            weighted_f1_.append(f1_score(test_y, pred, average = 'weighted'))
-            accuracy_.append(accuracy_score(test_y, pred))
-    
-        result_.append(dict(zip(metrics_, [params[i], 
-                                           np.mean(macro_precision_),
-                                           np.mean(weighted_precision_),
-                                           np.mean(macro_recall_),
-                                           np.mean(weighted_recall_),
-                                           np.mean(macro_f1_),
-                                           np.mean(weighted_f1_),
-                                           np.mean(accuracy_)])))
-    
-    return pd.DataFrame(result_)
+        val_macro_precision_.append(precision_score(val_y, val_pred, average = 'macro'))
+        val_weighted_precision_.append(precision_score(val_y, val_pred, average = 'weighted'))
+        val_macro_recall_.append(recall_score(val_y, val_pred, average = 'macro'))
+        val_weighted_recall_.append(recall_score(val_y, val_pred, average = 'weighted'))
+        val_macro_f1_.append(f1_score(val_y, val_pred, average = 'macro'))
+        val_weighted_f1_.append(f1_score(val_y, val_pred, average = 'weighted'))
+        val_accuracy_.append(accuracy_score(val_y, val_pred))
+
+    result_.append(dict(zip(list(params[i].keys())+ train_metrics + val_metrics, 
+                            list(params[i].values()) + 
+                            [np.mean(train_macro_precision_), np.mean(train_weighted_precision_),
+                             np.mean(train_macro_recall_), np.mean(train_weighted_recall_),
+                             np.mean(train_macro_f1_), np.mean(train_weighted_f1_),
+                             np.mean(train_accuracy_),
+                             np.mean(val_macro_precision_), np.mean(val_weighted_precision_),
+                             np.mean(val_macro_recall_), np.mean(val_weighted_recall_),
+                             np.mean(val_macro_f1_), np.mean(val_weighted_f1_),
+                             np.mean(val_accuracy_)])))
 
 
-result = gridCV(LogisticRegression, train_mgl_x, train_mgl_y, logit_params)
+result = pd.DataFrame(result_)
 result
+
+
+#%%
+from matplotlib import pyplot as plt
+
+
+logit_f1 = result.groupby(['C'])[['train_weighted_f1', 'val_weighted_f1']].mean().reset_index()
+
+
+plt.plot(logit_f1.C, logit_f1.train_weighted_f1)
+plt.plot(logit_f1.C, logit_f1.val_weighted_f1)
+plt.title('F1 score')
+plt.xlabel('C')
+plt.legend(['train', 'validation'])
+plt.show()
+plt.close()
